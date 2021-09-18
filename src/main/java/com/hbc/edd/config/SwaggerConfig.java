05b7c25cb273afad93f9d7d5f33b9f12ed44c180
venbabu <venkata.babu@publicissapient.com>
2021-09-18 15:16:52 -0500

    Adding SwaggerConfig

---
+++ src/main/java/com/hbc/edd/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package com.hbc.edd.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Collections.singletonList(apikey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return Collections.singletonList(new SecurityReference("Bearer", authorizationScopes));
+
+    }
+
+    private ApiKey apikey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "EDD Service",
+                "API Documentation",
+                "API 1.0",
+                "Terms of Service",
+                new Contact("", "www.thebay.com", ""),
+                "",
+                "",
+                Collections.emptyList());
+    }
+}
