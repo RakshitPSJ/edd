78f2b1506fb7ee4d2b3a2aef0a5d17d0fccc3e21
Rakshit Jain <rakjain1@publicisgroupe.net>
2021-09-21 12:43:24 +0530

    add change for exception

--- src/main/java/com/hbc/edd/eddService/controller/EDDController.java
+++ src/main/java/com/hbc/edd/eddService/controller/EDDController.java
@@ -40,7 +40,6 @@ public class EDDController {
                 || Strings.isNullOrEmpty(order.getBasketId())
                 || Strings.isNullOrEmpty(order.getOrganizationCode())
                 || Strings.isNullOrEmpty(order.getBasketId())) {
-            
             throw new InvalidRequest("Required fields are missing.");
         }
 
