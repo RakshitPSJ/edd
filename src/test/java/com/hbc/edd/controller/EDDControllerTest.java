ef7d7bc8fe1020703aef04c0f0cdaebf16f5d79f
venbabu <venkata.babu@publicissapient.com>
2021-09-20 20:56:57 -0500

    Adding Test-cases and EDD Request and Response POJO.

--- src/test/java/com/hbc/edd/controller/EDDControllerTest.java
+++ src/test/java/com/hbc/edd/controller/EDDControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
